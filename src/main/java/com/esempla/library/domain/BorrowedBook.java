80758f771aa9d6d4e49a61e0073b20c218f93db5
Leonid Caminschi <leonid.caminschi@gmail.com>
2025-04-01 11:45:43 +0300

    Added the Identity Sequence

--- src/main/java/com/esempla/library/domain/BorrowedBook.java
+++ src/main/java/com/esempla/library/domain/BorrowedBook.java
@@ -18,6 +18,7 @@ public class BorrowedBook implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
